054ded894d8690700b38b355facae1bfa8753dc5
Thinkman Wang <wangxf1985@gmail.com>
2020-02-24 00:49:23 +0800

    update

--- src/main/java/com/thinkman/springboot/mapper/Test1Mapper.java
+++ src/main/java/com/thinkman/springboot/mapper/Test1Mapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
-@DataSource(DataSourceType.THINKMAN)
+@DataSource(DataSourceType.SLAVE)
 public interface Test1Mapper {
 
     @Select("SELECT count(1) from t_test")
