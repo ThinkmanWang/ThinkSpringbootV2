5d8e03c37dd359d310bb8a5b055c52a21e41e2f4
Thinkman Wang <wangxf1985@gmail.com>
2020-02-24 00:01:00 +0800

    update

---
+++ src/main/java/com/thinkman/springboot/framework/aspecjt/lang/enums/DataSourceType.java
@@ -0,0 +1,24 @@
+package com.thinkman.springboot.framework.aspecjt.lang.enums;
+
+/**
+ * 数据源
+ * 
+ * @author ruoyi
+ */
+public enum DataSourceType
+{
+    /**
+     * 主库
+     */
+    MASTER,
+
+//    /**
+//     * 从库
+//     */
+//    SLAVE,
+//
+//    /**
+//     * THINKMAN
+//     */
+//    THINKMAN
+}
---
+++ src/main/java/com/thinkman/springboot/framework/config/DruidConfig.java
@@ -0,0 +1,69 @@
+package com.thinkman.springboot.framework.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties;
+import com.alibaba.druid.util.Utils;
+import com.thinkman.springboot.framework.aspecjt.lang.enums.DataSourceType;
+import com.thinkman.springboot.framework.datasource.DynamicDataSource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.servlet.*;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * druid 配置多数据源
+ * 
+ * @author ruoyi
+ */
+@Configuration
+public class DruidConfig
+{
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.master")
+    public DataSource masterDataSource(DruidProperties druidProperties)
+    {
+        DruidDataSource dataSource = DruidDataSourceBuilder.create().build();
+        return druidProperties.dataSource(dataSource);
+    }
+
+//    @Bean
+//    @ConfigurationProperties("spring.datasource.druid.slave")
+//    @ConditionalOnProperty(prefix = "spring.datasource.druid.slave", name = "enabled", havingValue = "true")
+//    public DataSource slaveDataSource(DruidProperties druidProperties)
+//    {
+//        DruidDataSource dataSource = DruidDataSourceBuilder.create().build();
+//        return druidProperties.dataSource(dataSource);
+//    }
+//
+//    @Bean
+//    @ConfigurationProperties("spring.datasource.druid.thinkman")
+//    @ConditionalOnProperty(prefix = "spring.datasource.druid.thinkman", name = "enabled", havingValue = "true")
+//    public DataSource thinkmanDataSource(DruidProperties druidProperties)
+//    {
+//        DruidDataSource dataSource = DruidDataSourceBuilder.create().build();
+//        return druidProperties.dataSource(dataSource);
+//    }
+
+    @Bean(name = "dynamicDataSource")
+    @Primary
+    public DynamicDataSource dataSource(DataSource masterDataSource
+            , DataSource slaveDataSource
+            , DataSource thinkmanDataSource)
+    {
+        Map<Object, Object> targetDataSources = new HashMap<>();
+        targetDataSources.put(DataSourceType.MASTER.name(), masterDataSource);
+//        targetDataSources.put(DataSourceType.SLAVE.name(), slaveDataSource);
+//        targetDataSources.put(DataSourceType.THINKMAN.name(), thinkmanDataSource);
+
+        return new DynamicDataSource(masterDataSource, targetDataSources);
+    }
+}
