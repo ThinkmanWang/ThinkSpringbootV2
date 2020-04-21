a2bd6b912572a65678360773b7a3f6e06947c7d4
Thinkman Wang <wangxf1985@gmail.com>
2020-04-21 17:21:40 +0800

    add heartbeat checker

---
+++ src/main/java/com/thinkman/thinkspringboot/project/netty/utils/HeartbeatChecker.java
@@ -0,0 +1,33 @@
+package com.thinkman.thinkspringboot.project.netty.utils;
+
+import com.thinkman.thinkspringboot.project.netty.tcpserver.MainTCPServer;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class HeartbeatChecker {
+    private static final Logger logger = Logger.getLogger(HeartbeatChecker.class);
+
+    private static final int HEARTBEAT_TIMEOUT = 10 * 1000;
+
+    @PostConstruct
+    public void start() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        logger.info("Start heartbeat check");
+
+                        Thread.sleep(HEARTBEAT_TIMEOUT);
+                    } catch (Exception ex) {
+
+                    }
+                }
+            }
+        }).start();
+    }
+
+}
